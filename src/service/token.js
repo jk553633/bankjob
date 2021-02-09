34eee89ffab2ef37861894543a601b70fc33a734
xiazhe <jk553633@163.com>
2021-02-09 17:28:48 +0900

    fixed

--- src/service/token.js
+++ src/service/token.js
@@ -1,9 +1,9 @@
-const jwt = require('jsonwebtoken'); // JWT
+const jwt = require('jsonwebtoken');
 const secret = '&w@ueJnXy!Gj0J@qVmMPR^9ip00EP0^Qy5Se#lzwStExJ3Zw4j02NVLm^btjBO8x';
 
 module.exports = class extends think.Service {
-  async create(userId) { // 创建令牌
-    return jwt.sign(userId, secret);
+  create(userId) { // 创建令牌
+    return jwt.sign(userId, secret, { expiresIn: 60 * 60 });
   }
 
   parse() { // 令牌验签
