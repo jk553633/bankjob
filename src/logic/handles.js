31d9ea32ecb383a1a93a12459fd42469ba81482d
xiazhe <jk553633@163.com>
2021-02-09 12:57:36 +0900

    handles

--- src/logic/handles.js
+++ src/logic/handles.js
@@ -2,4 +2,38 @@ module.exports = class extends think.Logic {
   indexAction() {
 
   }
+  getHandlesByParamsAction() {
+    this.allowMethods = 'get'; //  只允许 GET 请求类型
+    const rules = {
+      currentPage: {
+        required: true,
+        int: true // 需要是 int 型
+      },
+      pageSize: {
+        required: true,
+        int: true // 需要是 int 型
+      }
+    };
+    const flag = this.validate(rules);
+    if (!flag) {
+      return this.fail('validate error', this.validateErrors);
+    }
+  }
+  getHandlesAction() {
+    this.allowMethods = 'get'; //  只允许 GET 请求类型
+    const rules = {
+      currentPage: {
+        required: true,
+        int: true // 需要是 int 型
+      },
+      pageSize: {
+        required: true,
+        int: true // 需要是 int 型
+      }
+    };
+    const flag = this.validate(rules);
+    if (!flag) {
+      return this.fail('validate error', this.validateErrors);
+    }
+  }
 };
