34dfc4963e2f3df3baa832fe2928014a69301dec
xiazhe <jk553633@163.com>
2021-02-09 15:06:24 +0900

    handles

--- src/logic/tellers.js
+++ src/logic/tellers.js
@@ -2,4 +2,37 @@ module.exports = class extends think.Logic {
   indexAction() {
 
   }
+  loginAction() {
+    this.allowMethods = 'post'; //  只允许 POST 请求类型
+    const rules = {
+      account: {
+        required: true
+      },
+      password: {
+        required: true
+      }
+    };
+    const flag = this.validate(rules);
+    if (!flag) {
+      return this.fail('validate error', this.validateErrors);
+    }
+  }
+  registerAction() {
+    this.allowMethods = 'post'; //  只允许 POST 请求类型
+    const rules = {
+      account: {
+        required: true
+      },
+      name: {
+        required: true
+      },
+      password: {
+        required: true
+      }
+    };
+    const flag = this.validate(rules);
+    if (!flag) {
+      return this.fail('validate error', this.validateErrors);
+    }
+  }
 };
