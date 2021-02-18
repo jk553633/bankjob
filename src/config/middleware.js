7b148efb33eae01a4665378dd9682ab3fbba81cf
xiaz <jk553633@163.com>
2021-02-18 12:44:44 +0800

    koa-cors installed

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -1,7 +1,12 @@
 const path = require('path');
+const cors = require('koa-cors');
 const isDev = think.env === 'development';
 
 module.exports = [
+  {
+    handle: cors,
+    options: {}
+  },
   {
     handle: 'meta',
     options: {
