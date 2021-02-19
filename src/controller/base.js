5ff091ece8cb1c92fd710c725ba034e295f06d1d
xiaz <jk553633@163.com>
2021-02-19 09:29:14 +0800

    getNumber

--- src/controller/base.js
+++ src/controller/base.js
@@ -29,13 +29,13 @@ module.exports = class extends think.Controller {
    * @returns {Promise<any|void>}
    */
   getNumber() {
-    var time = new Date();
-    var year = time.getFullYear();
-    var month = time.getMonth() + 1;
-    var date = time.getDate();
-    var hours = time.getHours();
-    var minutes = time.getMinutes();
-    var seconds = time.getSeconds();
+    const time = new Date();
+    const year = time.getFullYear();
+    const month = time.getMonth() + 1;
+    const date = time.getDate();
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
     return year + this.add0(month) + this.add0(date) + this.add0(hours) + this.add0(minutes) + this.add0(seconds);
   }
 
